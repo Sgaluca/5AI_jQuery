e0f6046d89d9453bd3367a7ff8d7ef63ad90cce9
Sgarlato Luca <sgarlato.luca@osdbsesto>
2020-01-24 10:02:00 +0100

    modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.getElementById("id1").innerHTML = "ciao come stai";
+var btn = document.createElement("button");
+btn.innerHTML = "clicca";
+
+var body = document.getElementsByTagName("body")[0];
+body.append(btn);
+
+btn.addEventListener("click",function(){
+    document.getElementById("id1").innerHTML = "ciao come stai"
+});

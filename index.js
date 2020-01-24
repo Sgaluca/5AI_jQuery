707a466a7e9d4ae6f6ca190ee9fcd2dc06efc518
Sgarlato Luca <sgarlato.luca@osdbsesto>
2020-01-24 10:04:02 +0100

    add button and btn.eventlistener

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var body = document.getElementsByTagName("body")[0];
 body.append(btn);
 
 btn.addEventListener("click",function(){
-    document.getElementById("id1").innerHTML = "ciao come stai"
+    document.getElementById("id1").innerHTML = "ciao come stai?S"
 });
